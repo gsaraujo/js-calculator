5bc63b803b21bdf276df984df0dfb4b71ebcc974
Gustavo <sasaeucomvoce@hotmail.com>
2024-01-05 18:47:56 -0300

    feat: getters and setters

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,3 +1,25 @@
 class CalcController {
-    
+
+    constructor() {
+        this._displayCalc = "0";// _ "means private atrribute"
+        this._currentDate;
+
+    }
+
+    get displayCalc(){
+        return this._displayCalc;
+    }
+
+    set displayCalc(value) {
+        this._displayCalc = value;
+    }
+
+    get currentDate(){
+        return this._currentDate;
+    }
+
+    set currentDate(value){
+        this._currentDate = value;
+
+    }
 }
