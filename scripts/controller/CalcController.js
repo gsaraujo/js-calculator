e1c0800b9fabe1bb0b4c409f73eeddb3618ce01f
Gustavo <sasaeucomvoce@hotmail.com>
2024-01-08 11:40:30 -0300

    feat: double press on + and =

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,6 +2,8 @@ class CalcController {
 
     constructor(){
 
+        this._lastOperator = '';
+        this._lastNumber = '';
         this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
@@ -81,15 +83,38 @@ class CalcController {
 
     }
 
+    getResult() {
+
+        return eval(this._operation.join(""));
+
+    }
+
     calc(){
 
         let last = '';
 
-        if (this._operation.length > 3) {
-            last = this._operation.pop();
+        this._lastOperator = this.getLastItem(true);
+
+        if (this._operation.length < 3) {
+            
+            let firstItem = this._operation[0];
+            this._operation = [firstItem, this._lastOperator, this._lastNumber];
+
         }
+
+        if (this._operation.length > 3) {
+
+            last = this._operation.pop();            
+            this._lastNumber = this.getResult();
+
+        } else if (this._operation.length == 3) {
+            
+            this._lastNumber = this.getLastItem(false);
+            
+        }
+
         
-        let result = eval(this._operation.join(""));
+        let result = this.getResult();
 
         if (last == '%') {
 
@@ -112,16 +137,36 @@ class CalcController {
 
     }
 
-    setLastNumberToDisplay(){
+    getLastItem(isOperator = true) {
 
-        let lastNumber;
+        /**
+         * isOperator == true -> returns the last operator.
+         * isOperator == false -> returns the last number.
+         */
+        let lastItem;
 
         for (let i = this._operation.length-1; i>=0; i--) {
-            if (!this.isOperator(this._operation[i])) {
-                lastNumber = this._operation[i];
+            
+            if (this.isOperator(this._operation[i]) == isOperator) {
+                lastItem = this._operation[i];
                 break;
             }
+            
         }
+
+        if (!lastItem) {
+            
+            lastItem = (isOperator) ? this._lastOperator : this._lastNumber;
+
+        }
+
+        return lastItem;
+
+    }
+
+    setLastNumberToDisplay(){
+
+        let lastNumber = this.getLastItem(false);
 
         if (!lastNumber) lastNumber = 0;
 
@@ -133,8 +178,6 @@ class CalcController {
 
     addOperation(value){
 
-        console.log('A', value, isNaN(this.getLastOperation()));
-
         if (isNaN(this.getLastOperation())) {
 
             if (this.isOperator(value)) {
@@ -143,7 +186,7 @@ class CalcController {
 
             } else if (isNaN(value)){
 
-                console.log(value);
+                //console.log(value);
 
             } else {
 
@@ -169,8 +212,6 @@ class CalcController {
             }
 
         }
-
-        console.log(this._operation);
 
     }
 
