b451562620cb6f395cdb5dab4acd455bfaa86bf2
Gustavo <sasaeucomvoce@hotmail.com>
2024-01-16 22:26:42 -0300

    feat: adding try catch before eval expression

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -212,7 +212,19 @@ class CalcController {
 
     getResult() {
 
-        return eval(this._operation.join(""));
+        try {
+
+            return eval(this._operation.join(""));
+
+        } catch (e) {
+
+            setTimeout(()=>{
+
+                this.setError();
+
+            }, 1);
+
+        }
 
     }
 
